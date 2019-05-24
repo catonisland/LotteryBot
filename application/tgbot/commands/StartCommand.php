a8f3db1a97ebc7b527c31fb786a4ed24dc446a83
TingV <thinkphp6@gmail.com>
2019-05-25 05:31:43 +0800

    按 longman/telegram-bot Version 0.56.0 的要求，需要自己实现系统命令

---
+++ application/tgbot/commands/StartCommand.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This file is part of the TelegramBot package.
+ *
+ * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Longman\TelegramBot\Commands\SystemCommands;
+
+use Longman\TelegramBot\Commands\SystemCommand;
+use Longman\TelegramBot\Request;
+
+/**
+ * Start command
+ *
+ * @todo Remove due to deprecation!
+ */
+class StartCommand extends SystemCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'start';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Start command';
+
+    /**
+     * @var string
+     */
+    protected $usage = '/start';
+
+    /**
+     * @var string
+     */
+    protected $version = '1.0.0';
+
+    /**
+     * Command execute method
+     *
+     * @return mixed
+     * @throws \Longman\TelegramBot\Exception\TelegramException
+     */
+    public function execute()
+    {
+        $message = $this->getMessage();            // Get Message object
+
+        $chat_id = $message->getChat()->getId();   // Get the current Chat ID
+
+        $data = [                                  // Set up the new message data
+            'chat_id' => $chat_id,                 // Set Chat ID to send the message to
+            'text'    => 'This is just a Start...', // Set message to send
+        ];
+
+        return Request::sendMessage($data);        // Send message!
+    }
+}
