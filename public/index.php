9f68a81596ed0af0a4e4b2186e7d3376954dc84f
TingV <thinkphp6@gmail.com>
2019-05-25 01:33:58 +0800

    定义应用目录

--- public/index.php
+++ public/index.php
@@ -16,6 +16,9 @@ if (version_compare(PHP_VERSION, '5.6', '<')) {
     die('PHP版本过低，最少需要PHP5.6，请升级PHP版本！');
 }
 
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/../application/');
+
 // 定义后台入口文件
 define('ADMIN_FILE', 'admin.php');
 
