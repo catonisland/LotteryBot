e72eaeb4e002c1dc69c986366e5c59a12c3b8f33
TingV <thinkphp6@gmail.com>
2019-05-25 01:16:22 +0800

    Initial commit

---
+++ application/admin/controller/System.php
@@ -0,0 +1,153 @@
+<?php
+// +----------------------------------------------------------------------
+// | 海豚PHP框架 [ DolphinPHP ]
+// +----------------------------------------------------------------------
+// | 版权所有 2016~2019 广东卓锐软件有限公司 [ http://www.zrthink.com ]
+// +----------------------------------------------------------------------
+// | 官方网站: http://dolphinphp.com
+// +----------------------------------------------------------------------
+
+namespace app\admin\controller;
+
+use app\common\builder\ZBuilder;
+use app\admin\model\Config as ConfigModel;
+use app\admin\model\Module as ModuleModel;
+
+/**
+ * 系统模块控制器
+ * @package app\admin\controller
+ */
+class System extends Admin
+{
+    /**
+     * 系统设置
+     * @param string $group 分组
+     * @author 蔡伟明 <314013107@qq.com>
+     * @return mixed
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function index($group = 'base')
+    {
+        // 保存数据
+        if ($this->request->isPost()) {
+            $data = $this->request->post();
+
+            if (isset(config('config_group')[$group])) {
+                // 查询该分组下所有的配置项名和类型
+                $items = ConfigModel::where('group', $group)->where('status', 1)->column('name,type');
+
+                foreach ($items as $name => $type) {
+                    if (!isset($data[$name])) {
+                        switch ($type) {
+                            // 开关
+                            case 'switch':
+                                $data[$name] = 0;
+                                break;
+                            case 'checkbox':
+                                $data[$name] = '';
+                                break;
+                        }
+                    } else {
+                        // 如果值是数组则转换成字符串，适用于复选框等类型
+                        if (is_array($data[$name])) {
+                            $data[$name] = implode(',', $data[$name]);
+                        }
+                        switch ($type) {
+                            // 开关
+                            case 'switch':
+                                $data[$name] = 1;
+                                break;
+                            // 日期时间
+                            case 'date':
+                            case 'time':
+                            case 'datetime':
+                                $data[$name] = strtotime($data[$name]);
+                                break;
+                        }
+                    }
+                    ConfigModel::where('name', $name)->update(['value' => $data[$name]]);
+                }
+            } else {
+                // 保存模块配置
+                if (false === ModuleModel::where('name', $group)->update(['config' => json_encode($data)])) {
+                    $this->error('更新失败');
+                }
+                // 非开发模式，缓存数据
+                if (config('develop_mode') == 0) {
+                    cache('module_config_'.$group, $data);
+                }
+            }
+            cache('system_config', null);
+            // 记录行为
+            action_log('system_config_update', 'admin_config', 0, UID, "分组($group)");
+            $this->success('更新成功', url('index', ['group' => $group]));
+        } else {
+            // 配置分组信息
+            $list_group = config('config_group');
+
+            // 读取模型配置
+            $modules = ModuleModel::where('config', 'neq', '')
+                ->where('status', 1)
+                ->column('config,title,name', 'name');
+            foreach ($modules as $name => $module) {
+                $list_group[$name] = $module['title'];
+            }
+
+            $tab_list = [];
+            foreach ($list_group as $key => $value) {
+                $tab_list[$key]['title'] = $value;
+                $tab_list[$key]['url']   = url('index', ['group' => $key]);
+            }
+
+            if (isset(config('config_group')[$group])) {
+                // 查询条件
+                $map['group']  = $group;
+                $map['status'] = 1;
+
+                // 数据列表
+                $data_list = ConfigModel::where($map)
+                    ->order('sort asc,id asc')
+                    ->column('name,title,tips,type,value,options,ajax_url,next_items,param,table,level,key,option,ak,format');
+
+                foreach ($data_list as &$value) {
+                    // 解析options
+                    if ($value['options'] != '') {
+                        $value['options'] = parse_attr($value['options']);
+                    }
+                }
+
+                // 默认模块列表
+                if (isset($data_list['home_default_module'])) {
+                    $list_module['index'] = '默认';
+                    $data_list['home_default_module']['options'] = array_merge($list_module, ModuleModel::getModule());
+                }
+
+                // 使用ZBuilder快速创建表单
+                return ZBuilder::make('form')
+                    ->setPageTitle('系统设置')
+                    ->setTabNav($tab_list, $group)
+                    ->setFormItems($data_list)
+                    ->fetch();
+            } else {
+                // 模块配置
+                $module_info = ModuleModel::getInfoFromFile($group);
+                $config      = $module_info['config'];
+                $trigger     = isset($module_info['trigger']) ? $module_info['trigger'] : [];
+
+                // 数据库内的模块信息
+                $db_config = ModuleModel::where('name', $group)->value('config');
+                $db_config = json_decode($db_config, true);
+
+                // 使用ZBuilder快速创建表单
+                return ZBuilder::make('form')
+                    ->setPageTitle('模块设置')
+                    ->setTabNav($tab_list, $group)
+                    ->addFormItems($config)
+                    ->setFormdata($db_config) // 设置表格数据
+                    ->setTrigger($trigger) // 设置触发
+                    ->fetch();
+            }
+        }
+    }
+}
---
+++ vendor/longman/telegram-bot/src/Entities/ServerResponse.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * This file is part of the TelegramBot package.
+ * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Longman\TelegramBot\Entities;
+
+use Longman\TelegramBot\Entities\Games\GameHighScore;
+use Longman\TelegramBot\Request;
+
+/**
+ * Class ServerResponse
+ *
+ * @link https://core.telegram.org/bots/api#making-requests
+ *
+ * @method bool   getOk()          If the request was successful
+ * @method mixed  getResult()      The result of the query
+ * @method int    getErrorCode()   Error code of the unsuccessful request
+ * @method string getDescription() Human-readable description of the result / unsuccessful request
+ *
+ * @todo method ResponseParameters getParameters()  Field which can help to automatically handle the error
+ */
+class ServerResponse extends Entity
+{
+    /**
+     * ServerResponse constructor.
+     *
+     * @param array  $data
+     * @param string $bot_username
+     *
+     * @throws \Longman\TelegramBot\Exception\TelegramException
+     */
+    public function __construct(array $data, $bot_username)
+    {
+        // Make sure we don't double-save the raw_data
+        unset($data['raw_data']);
+        $data['raw_data'] = $data;
+
+        $is_ok  = isset($data['ok']) ? (bool) $data['ok'] : false;
+        $result = isset($data['result']) ? $data['result'] : null;
+
+        if ($is_ok && is_array($result)) {
+            if ($this->isAssoc($result)) {
+                $data['result'] = $this->createResultObject($result, $bot_username);
+            } else {
+                $data['result'] = $this->createResultObjects($result, $bot_username);
+            }
+        }
+
+        parent::__construct($data, $bot_username);
+    }
+
+    /**
+     * Check if array is associative
+     *
+     * @link https://stackoverflow.com/a/4254008
+     *
+     * @param array $array
+     *
+     * @return bool
+     */
+    protected function isAssoc(array $array)
+    {
+        return count(array_filter(array_keys($array), 'is_string')) > 0;
+    }
+
+    /**
+     * If response is ok
+     *
+     * @return bool
+     */
+    public function isOk()
+    {
+        return (bool) $this->getOk();
+    }
+
+    /**
+     * Print error
+     *
+     * @see https://secure.php.net/manual/en/function.print-r.php
+     *
+     * @param bool $return
+     *
+     * @return bool|string
+     */
+    public function printError($return = false)
+    {
+        $error = sprintf('Error N: %s, Description: %s', $this->getErrorCode(), $this->getDescription());
+
+        if ($return) {
+            return $error;
+        }
+
+        echo $error;
+
+        return true;
+    }
+
+    /**
+     * Create and return the object of the received result
+     *
+     * @param array  $result
+     * @param string $bot_username
+     *
+     * @return \Longman\TelegramBot\Entities\Chat|\Longman\TelegramBot\Entities\ChatMember|\Longman\TelegramBot\Entities\File|\Longman\TelegramBot\Entities\Message|\Longman\TelegramBot\Entities\User|\Longman\TelegramBot\Entities\UserProfilePhotos|\Longman\TelegramBot\Entities\WebhookInfo
+     */
+    private function createResultObject(array $result, $bot_username)
+    {
+        $action = Request::getCurrentAction();
+
+        // We don't need to save the raw_data of the response object!
+        $result['raw_data'] = null;
+
+        $result_object_types = [
+            'getChat'              => Chat::class,
+            'getChatMember'        => ChatMember::class,
+            'getFile'              => File::class,
+            'getMe'                => User::class,
+            'getStickerSet'        => StickerSet::class,
+            'getUserProfilePhotos' => UserProfilePhotos::class,
+            'getWebhookInfo'       => WebhookInfo::class,
+        ];
+
+        $object_class = array_key_exists($action, $result_object_types) ? $result_object_types[$action] : Message::class;
+
+        return new $object_class($result, $bot_username);
+    }
+
+    /**
+     * Create and return the objects array of the received result
+     *
+     * @param array  $result
+     * @param string $bot_username
+     *
+     * @return \Longman\TelegramBot\Entities\ChatMember[]|\Longman\TelegramBot\Entities\Games\GameHighScore[]|\Longman\TelegramBot\Entities\Message[]|\Longman\TelegramBot\Entities\Update[]
+     */
+    private function createResultObjects(array $result, $bot_username)
+    {
+        $results = [];
+        $action  = Request::getCurrentAction();
+
+        $result_object_types = [
+            'getChatAdministrators' => ChatMember::class,
+            'getGameHighScores'     => GameHighScore::class,
+            'sendMediaGroup'        => Message::class,
+        ];
+
+        $object_class = array_key_exists($action, $result_object_types) ? $result_object_types[$action] : Update::class;
+
+        foreach ($result as $data) {
+            // We don't need to save the raw_data of the response object!
+            $data['raw_data'] = null;
+
+            $results[] = new $object_class($data, $bot_username);
+        }
+
+        return $results;
+    }
+}
