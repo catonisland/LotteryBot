ca4b6bb5ce24bf45fb87549f4dbd2d065c74bce9
TingV <oslaravel@gmail.com>
2019-05-25 20:18:52 +0800

    去掉一些没用的代码，否则定时任务无法执行

--- public/index.php
+++ public/index.php
@@ -10,12 +10,6 @@
 // [ 应用入口文件 ]
 namespace think;
 
-// [ PHP版本检查 ]
-header("Content-type: text/html; charset=utf-8");
-if (version_compare(PHP_VERSION, '5.6', '<')) {
-    die('PHP版本过低，最少需要PHP5.6，请升级PHP版本！');
-}
-
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 
@@ -27,11 +21,5 @@ require __DIR__ . '/../thinkphp/base.php';
 
 // 支持事先使用静态方法设置Request对象和Config对象
 
-// 检查是否安装
-if(!is_file('../data/install.lock')){
-    define('BIND_MODULE', 'install');
-    Container::get('app')->bind('install')->run()->send();
-} else {
-    // 执行应用并响应
-    Container::get('app')->run()->send();
-}
+// 执行应用并响应
+Container::get('app')->run()->send();
