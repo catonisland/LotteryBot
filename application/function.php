e5aec46c7e253239bdb3c0b8c3d59dccb472fc6e
TingV <oslaravel@gmail.com>
2019-05-25 19:24:34 +0800

    生成一定数量的不重复随机数，用来开奖

--- application/function.php
+++ application/function.php
@@ -9,3 +9,21 @@
 
 // 为方便系统核心升级，二次开发中需要用到的公共函数请写在这个文件，不要去修改common.php文件
 
+/*
+* 生成一定数量的不重复随机数
+* $min 和 $max: 指定随机数的范围
+* $num: 指定生成数量
+*/
+if (!function_exists('unique_rand')) {
+    function unique_rand($min, $max, $num){
+        $count = 0;
+        $return_arr = array();
+        while($count < $num){
+            $return_arr[] = mt_rand($min, $max);
+            $return_arr = array_flip( array_flip( $return_arr ) );
+            $count = count( $return_arr );
+        }
+        shuffle( $return_arr );
+        return $return_arr;
+    }
+}
